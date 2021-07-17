bfb7b1aecd86bbc2edc764c16778902dac8ff049
AbdulrhamnSkout <abd0525656@gmail.com>
2021-07-17 19:04:08 +0300

    create

---
+++ main.js
@@ -0,0 +1,31 @@
+/// <reference path="jquery-3.6.0.js" />
+"use strict";
+(function (){
+    
+    
+    // function changeBackground(){
+    //     let i=0;
+    //     let arr=["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
+    //     setInterval(
+    //     () => {
+    //        i=(i+1)%4;    
+    //         $("#header").fadeOut("slow", function () {
+    //             $(this).css("background-image", `url(asset/images/${arr[i]})`)
+    //             $(this).fadeIn("fast");
+    //        }) 
+    //     },4000);
+    // }
+
+    // changeBackground()
+
+
+
+
+
+
+
+
+
+
+
+})();
